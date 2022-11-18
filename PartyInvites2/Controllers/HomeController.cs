77259267ecae58204dc01ac78342a0f4acfdacc4
Lenty Sprangers <lenty_sprangeres@hotmail.com>
2022-11-18 18:01:58 +0100

    project init

---
+++ PartyInvites2/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartyInvites2.Models;
+
+namespace PartyInvites2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid) {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            } else {
+                return View();
+            }
+        }
+        public ViewResult ListResponses() {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
+    }
+}
---
+++ PartyInvites2/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PartyInvites2.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
