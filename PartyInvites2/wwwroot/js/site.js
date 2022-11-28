67ae7c150fbd7db327562114ccba971aec9803f0
Lenty Sprangers <lenty_sprangeres@hotmail.com>
2022-11-29 00:54:23 +0100

    fixed navbar

--- PartyInvites2/wwwroot/js/site.js
+++ PartyInvites2/wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+let zeroFoodwaste = document.getElementById("zero-foodwaste");
+let navbar = document.getElementById("main-nav");
+
+let navPos = navbar.getBoundingClientRect().top;
+
+window.addEventListener("scroll", e => {
+    let scrollPos = window.scrollY;
+    if (scrollPos > navPos) {
+        navbar.classList.add('sticky');
+        header.classList.add('navbarOffsetMargin');
+    } else {
+        navbar.classList.remove('sticky');
+        header.classList.remove('navbarOffsetMargin');
+    }
+});
